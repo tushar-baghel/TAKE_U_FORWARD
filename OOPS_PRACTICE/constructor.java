98cf1044a35073717c1a8a09a18730cac6a2b66c
tushar-baghel <tusharbaghel704@gmail.com>
2025-05-29 22:24:16 +0530

    Constructor practice

---
+++ OOPS_PRACTICE/constructor.java
@@ -0,0 +1,16 @@
+public class constructor {
+
+    int id;
+    String name ;
+
+    public static void main(String[] args){
+        constructor obj1 = new constructor();
+        obj1.id = 1;
+        obj1.name = "Shri Radhey";
+
+        System.out.println(obj1.id);
+        System.out.println(obj1.name);
+    }
+
+    
+}
